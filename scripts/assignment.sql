310b9993dc17b3ada6c7f071c55ea37de50c2aa3
Erica Ranck <ericaranck1@gmail.com>
2023-02-14 20:29:22 -0600

    4a

--- scripts/assignment.sql
+++ scripts/assignment.sql
@@ -90,11 +90,23 @@ GROUP BY drug_name
 ORDER BY daily_cost DESC
 LIMIT 1;
 
-
 -- chenodal is the priciest per day at $86,741
 
 -- 4. 
 --     a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
+
+SELECT 
+	DISTINCT drug_name,
+	CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
+	WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+	ELSE 'neither' END AS drug_type
+FROM drug;
+3260 results
+
+SELECT 
+	DISTINCT drug_name
+FROM drug;
+3253 results
 
 --     b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
 
